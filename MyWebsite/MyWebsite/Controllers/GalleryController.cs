70276f5fd765354a0de0f47dec2528fb92464ca2
Michael Gordeev <michael.xfox@outlook.com>
2019-10-10 20:19:12 +0300

    Implemented gallery

--- MyWebsite/MyWebsite/Controllers/GalleryController.cs
+++ MyWebsite/MyWebsite/Controllers/GalleryController.cs
@@ -1,13 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyWebsite.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MyWebsite.Controllers
 {
     public class GalleryController : Controller
     {
-        public IActionResult Index() =>
-            View();
 
-        public IActionResult Details(string id) =>
-            View(model: id);
+        public async Task<IActionResult> Index()
+        {
+            ViewData["Images"] = JsonConvert.DeserializeObject<Image[]>(await new HttpClient().GetStringAsync($"https://{Request.Host}/Gallery.json"));
+
+            return View();
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            ViewData["CurrentImage"] = JsonConvert.DeserializeObject<Image[]>(await new HttpClient().GetStringAsync($"https://{Request.Host}/Gallery.json")).First(i => i.FileName == id);
+
+            return View();
+        }
+            
     }
 }
---
+++ MyWebsite/MyWebsite/Models/Image.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyWebsite.Models
+{
+    public class Image
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string FileName { get; set; }
+    }
+}
