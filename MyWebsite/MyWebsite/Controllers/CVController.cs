c0cda901fca08e39671f12f65fb73b0ef84a5807
Michael Gordeev <michael.xfox@outlook.com>
2019-10-13 20:18:20 +0300

    Added CV page (content is not edited) Updated projects page

---
+++ MyWebsite/MyWebsite/Controllers/CVController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebsite.Controllers
+{
+    public class CVController : Controller
+    {
+        public IActionResult Index() =>
+            View();
+    }
+}
