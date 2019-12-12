d66b874de1eda3ea6976171b770785d4181a1e45
Michael Gordeev <michael.xfox@outlook.com>
2019-12-12 18:06:33 +0300

    Updated admin panel

--- MyWebsite/MyWebsite/Areas/Admin/Controllers/ContactsController.cs
+++ MyWebsite/MyWebsite/Areas/Admin/Controllers/ContactsController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MyWebsite.Models;
 
 namespace MyWebsite.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize]
     public class ContactsController : Controller
     {
         public ContactsController(DatabaseContext context) =>
