21f4021fa4b12df242f47a44a8b886708319f7c5
Michael Gordeev <michael.xfox@outlook.com>
2019-12-14 00:40:34 +0300

    Done resume management pages

---
+++ MyWebsite/MyWebsite/Areas/Admin/Controllers/ResumeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyWebsite.Models;
+using System;
+
+namespace MyWebsite.Areas.Admin.Controllers
+{
+    [Authorize]
+    [Area("Admin")]
+    public class ResumeController : Controller
+    {
+        public ResumeController(DatabaseContext context) =>
+            Startup.Database = context;
+
+        public IActionResult Index() =>
+            View(Startup.Database.Resume);
+
+        [HttpGet]
+        public IActionResult Edit(string id) =>
+            View(Startup.Database.Resume.Find(id));
+
+        [HttpPost]
+        public IActionResult Edit(Resume model)
+        {
+            model.LastUpdate = DateTime.Now;
+            Startup.Database.Resume.Update(model);
+            Startup.Database.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(string id) =>
+            View(Startup.Database.Resume.Find(id));
+
+        [HttpPost]
+        public IActionResult Delete(Resume model)
+        {
+            Startup.Database.Resume.Remove(model);
+            Startup.Database.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Create() =>
+            View();
+
+        [HttpPost]
+        public IActionResult Create(Resume model)
+        {
+            model.LastUpdate = DateTime.Now;
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("Error", "Invalid data");
+                return View(model);
+            }
+
+            Startup.Database.Resume.Add(model);
+            Startup.Database.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- MyWebsite/MyWebsite/Models/Resume.cs
+++ MyWebsite/MyWebsite/Models/Resume.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyWebsite.Models
@@ -6,10 +8,16 @@ namespace MyWebsite.Models
     public class Resume
     {
         [Key]
+        [Required]
         [Column(TypeName = "varchar(10)")]
+        [DisplayName("Language")]
         public string Language { get; set; }
         [Required]
         [Column(TypeName = "text")]
+        [DisplayName("Content")]
         public string Content { get; set; }
+        [DisplayName("Last chagnge")]
+        public DateTime LastUpdate { get; set; }
+
     }
 }
