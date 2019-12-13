eaeb606e7ee816ba4c101f2197a3421af4fccf42
Michael Gordeev <michael.xfox@outlook.com>
2019-12-13 19:45:54 +0300

    Gallery management list done

---
+++ MyWebsite/MyWebsite/Areas/Admin/Controllers/GalleryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyWebsite.Models;
+
+namespace MyWebsite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class GalleryController : Controller
+    {
+        public GalleryController(DatabaseContext context) =>
+            Startup.Database = context;
+
+        public IActionResult Index() =>
+            View(Startup.Database.Gallery);
+
+
+    }
+}
