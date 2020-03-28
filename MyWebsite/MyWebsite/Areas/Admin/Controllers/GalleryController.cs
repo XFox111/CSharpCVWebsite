6c1545b0565f5ae37dc1dde7678a92c940850cae
Michael Gordeev <michael.xfox@outlook.com>
2020-03-28 18:04:40 +0300

    Added gallery artworks caching

--- MyWebsite/MyWebsite/Areas/Admin/Controllers/GalleryController.cs
+++ MyWebsite/MyWebsite/Areas/Admin/Controllers/GalleryController.cs
@@ -6,6 +6,8 @@ using MyWebsite.Models;
 using MyWebsite.Models.Databases;
 using System;
 using System.IO;
+using System.Drawing;
+using System.Threading;
 
 namespace MyWebsite.Areas.Admin.Controllers
 {
@@ -50,6 +52,8 @@ namespace MyWebsite.Areas.Admin.Controllers
 			System.IO.File.SetAttributes(Directory.GetCurrentDirectory() + "/wwwroot/images/Gallery/" + model?.FileName, FileAttributes.Normal);
 			System.IO.File.Delete(Directory.GetCurrentDirectory() + "/wwwroot/images/Gallery/" + model?.FileName);
 
+			UpdateCache();
+
 			return RedirectToAction("Index");
 		}
 
@@ -78,6 +82,39 @@ namespace MyWebsite.Areas.Admin.Controllers
 
 			Database.Gallery.Add(model);
 			Database.SaveChanges();
+
+			UpdateCache();
+
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+		public IActionResult UpdateCache()
+		{
+			if (Directory.Exists(Directory.GetCurrentDirectory() + "/wwwroot/images/Gallery/Cache"))
+				Directory.Delete(Directory.GetCurrentDirectory() + "/wwwroot/images/Gallery/Cache", true);
+
+			Thread.Sleep(10);
+			DirectoryInfo cache = Directory.CreateDirectory(Directory.GetCurrentDirectory() + "/wwwroot/images/Gallery/Cache");
+
+			foreach (string file in Directory.GetFiles(Directory.GetCurrentDirectory() + "/wwwroot/images/Gallery"))
+			{
+				if (file.EndsWith(".svg", StringComparison.OrdinalIgnoreCase))
+					continue;
+
+				FileInfo info = new FileInfo(file);
+
+				using Bitmap original = new Bitmap(file);
+				using Bitmap resized = new Bitmap(600, (int)(600d / original.Width * original.Height));
+				using Graphics grapics = Graphics.FromImage(resized);
+
+				grapics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighSpeed;
+				grapics.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceCopy;
+				grapics.DrawImage(original, 0, 0, resized.Width, resized.Height);
+
+				using Stream writeStream = System.IO.File.Open(cache.FullName + "\\" + info.Name + ".jpg", FileMode.Create);
+				resized.Save(writeStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+			}
 
 			return RedirectToAction("Index");
 		}
